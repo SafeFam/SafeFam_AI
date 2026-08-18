1d7afa530b01a189c079a4c49694701d380a73e6
pearseona <baesuna2930@gmail.com>
2026-08-18 15:38:34 +0900

    fix: allow the real_holdout source in the diversity merge tool

--- data_science/SMSModel/merge_sms_diversity.py
+++ data_science/SMSModel/merge_sms_diversity.py
@@ -23,7 +23,7 @@ BASE_SOURCES = {
     "original", "user_added", "synthetic_new_holdout", "synthetic_fp_stress",
     "synthetic_fp_stress_train", "reviewed_reclassification_v2",
     "public_phishing_v2", "synthetic_diversity_v2", "synthetic_hard_negative_v2",
-    "synthetic_normal_v3",
+    "synthetic_normal_v3", "real_holdout",
 }
 ADDITION_SOURCES = {
     "public_phishing_v2", "synthetic_diversity_v2",
